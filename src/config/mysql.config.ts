98a2bbd86bc9e566fdf4d120f7400e4314cb0a50
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-03-25 21:03:32 +0200

    add MySQL port

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -13,6 +13,7 @@ export class MySQLDatabaseClient implements IDatabaseClient {
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE_NAME,
+      port: Number(process.env.MYSQL_PORT),
     });
   }
 
