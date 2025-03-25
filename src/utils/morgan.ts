b173558c1d2f98ae8659a20e4760ba102ed12744
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-03-25 20:15:44 +0200

    remove unused morgan logger

--- src/utils/morgan.ts
+++
@@ -1,8 +0,0 @@
-import morgan from 'morgan';
-
-export const httpRequestLogger =
-  process.env.NODE_ENV === 'development'
-    ? morgan('dev')
-    : morgan('combined', {
-        skip: (_, res) => res.statusCode < 500,
-      });
